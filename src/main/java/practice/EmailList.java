b453a79b65ca9104808ece4b6acef4e876a80ff3
r_aru <r_arutkina@mail.ru>
2025-06-30 14:07:00 +0900

    feat: implement email list console app with validation and sorting

--- src/main/java/practice/EmailList.java
+++ src/main/java/practice/EmailList.java
@@ -1,19 +1,34 @@
 package practice;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
 
 public class EmailList {
 
+    TreeSet<String> emails = new TreeSet<>();
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[\\w\\.-]+@[\\w\\.-]+\\.[a-z]{2,}$",
+            Pattern.CASE_INSENSITIVE);
+
     public void add(String email) {
-        // TODO: валидный формат email добавляется, email это строка, она быть может любой
-        // принять решение добавлять аргумент email или нет должен этот метод
+
+        if (isValidEmail (email)) {
+            emails.add(email.toLowerCase());
+        } else {
+            System.out.println("Некорректный формат email: " + email);
+        }
+    }
+    private boolean isValidEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
     }
 
     public List<String> getSortedEmails() {
-        // TODO: возвращается сортированный список электронных адресов в алфавитном порядке
-        return new ArrayList<>();
+
+        return new ArrayList<>(emails);
+
+
     }
 
 }
+
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,33 +1,36 @@
+import practice.EmailList;
+
 import java.util.Scanner;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
-    /* TODO:
-        Пример вывода списка Email, после ввода команды LIST в консоль:
-        test@test.com
-        hello@mail.ru
-        - каждый адрес с новой строки
-        - список должен быть отсортирован по алфавиту
-        - email в разных регистрах считается одинаковыми
-           hello@skillbox.ru == HeLLO@SKILLbox.RU
-        - вывод на печать должен быть в нижнем регистре
-           hello@skillbox.ru
-        Пример вывода сообщения об ошибке при неверном формате Email:
-        "Неверный формат email"
-    */
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        EmailList emailList = new EmailList();
+
         while (true) {
+            System.out.println("Введите команду: ");
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
             }
-            
-            //TODO: write code here
-            
+            String[] parts = input.split(" ", 2);
+            String command = parts[0].toUpperCase();
+            switch (command) {
+                case "ADD":
+                    if (parts.length > 1) {
+                        emailList.add(parts[1]);
+                    } else {
+                        System.out.println("Ошибка: email не введён");
+                    }
+                    break;
+                case "LIST":
+                    emailList.getSortedEmails();
+                    break;
+            }
+
         }
     }
 }
+
